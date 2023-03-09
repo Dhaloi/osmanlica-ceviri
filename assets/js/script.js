c7668587b9119c28077d91590205176221175f2b
Dhaloi <69165505+Dhaloi@users.noreply.github.com>
2023-03-09 22:23:12 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,198 @@
+var userName; 
+function replaceName() {
+userName = document.getElementById('userNameTxt').value; 
+userName  = userName.replace(/H/g, "ح");
+userName  = userName.replace(/Z/g, "ظ");
+userName  = userName.replace(/T/g, "ط");
+userName  = userName.replace(/İ/g, "ى");
+userName  = userName.replace(/a  /g, "ه ");
+userName  = userName.replace(/i /g, "ى ");
+userName  = userName.replace(/ı /g, "ى ");
+userName  = userName.replace(/ i/g, " اي");
+userName  = userName.replace(/e /g, "ه ");
+userName  = userName.replace(/S/g, "ص");
+userName = userName.replace(/A/g, "آ");
+userName = userName.replace(/a/g, "ا");
+userName  = userName.replace(/w/g, "ع");
+userName  = userName.replace(/ o/g, " او");
+userName  = userName.replace(/ ö/g, " او");
+userName  = userName.replace(/ u/g, " او");
+userName  = userName.replace(/ ü/g, " او");
+userName  = userName.replace(/</g, "ه ");
+userName  = userName.replace(/b/g, "ب");
+userName  = userName.replace(/B/g, "ب");
+userName  = userName.replace(/p/g, "پ");
+userName  = userName.replace(/P/g, "پ");
+userName  = userName.replace(/t/g, "ت");
+userName  = userName.replace(/T/g, "ت");
+userName  = userName.replace(/c/g, "ج");
+userName  = userName.replace(/C/g, "ج");
+userName  = userName.replace(/ç/g, "چ");
+userName  = userName.replace(/Ç/g, "چ");
+userName  = userName.replace(/x/g, "خ");
+userName  = userName.replace(/X/g, "خ");
+userName  = userName.replace(/d/g, "د");
+userName  = userName.replace(/D/g, "ض");
+userName  = userName.replace(/r/g, "ر");
+userName  = userName.replace(/R/g, "ر");
+userName  = userName.replace(/z/g, "ز");
+userName  = userName.replace(/Z/g, "ز");
+userName  = userName.replace(/j/g, "ژ");
+userName  = userName.replace(/J/g, "ژ");
+userName  = userName.replace(/s/g, "س");
+userName  = userName.replace(/S/g, "س");
+userName  = userName.replace(/ş/g, "ش");
+userName  = userName.replace(/Ş/g, "ش");
+userName  = userName.replace(/f/g, "ف");
+userName  = userName.replace(/F/g, "ف");
+userName  = userName.replace(/q/g, "ق");
+userName  = userName.replace(/Q/g, "ق");
+userName  = userName.replace(/k/g, "ك");
+userName  = userName.replace(/K/g, "ك");
+userName  = userName.replace(/Ğ/g, "ك");
+userName  = userName.replace(/g/g, "ك");
+userName  = userName.replace(/G/g, "ك");
+userName  = userName.replace(/l/g, "ل");
+userName  = userName.replace(/L/g, "ل");
+userName  = userName.replace(/m/g, "م");
+userName  = userName.replace(/M/g, "م");
+userName  = userName.replace(/n/g, "ن");
+userName  = userName.replace(/N/g, "ن");
+userName  = userName.replace(/v/g, "و");
+userName  = userName.replace(/V/g, "و");
+userName  = userName.replace(/w/g, "و");
+userName  = userName.replace(/W/g, "و");
+userName  = userName.replace(/o/g, "و");
+userName  = userName.replace(/u/g, "و");
+userName  = userName.replace(/U/g, "و");
+userName  = userName.replace(/ü/g, "و");
+userName  = userName.replace(/ö/g, "و");
+userName  = userName.replace(/O/g, "و");
+userName  = userName.replace(/Ö/g, "و");
+userName  = userName.replace(/Ü/g, "و");
+userName  = userName.replace(/h/g, "ه");
+userName  = userName.replace(/H/g, "ه");
+userName  = userName.replace(/e/g, "");
+userName  = userName.replace(/E/g, "ا");
+userName  = userName.replace(/,/g, "،");
+userName  = userName.replace(/;/g, "؛");
+userName  = userName.replace(/ğ/g, "غ");
+userName  = userName.replace(/â/g, "آ");
+userName  = userName.replace(/æ/g, "ث");
+userName  = userName.replace(/Â/g, "آ");
+userName  = userName.replace(/å/g, "ث");
+userName  = userName.replace(/Ó/g, "ح");
+userName  = userName.replace(/ó/g, "ح");
+userName  = userName.replace(/Ô/g, "ظ");
+userName  = userName.replace(/ô/g, "ظ");
+userName  = userName.replace(/Õ/g, "ذ");
+userName  = userName.replace(/õ/g, "ذ");
+userName  = userName.replace(/Ù/g, "ط");
+userName  = userName.replace(/ù/g, "ط");
+userName  = userName.replace(/Ú/g, "ق");
+userName  = userName.replace(/ú/g, "ق");
+userName  = userName.replace(/Û/g, "و");
+userName  = userName.replace(/û/g, "و");
+userName  = userName.replace(/Ò/g, "خ");
+userName  = userName.replace(/ò/g, "خ");
+userName  = userName.replace(/á/g, "غ");
+userName  = userName.replace(/à/g, "غ");
+userName  = userName.replace(/ä/g, "ص");
+userName  = userName.replace(/ã/g, "ص");
+userName  = userName.replace(/ë/g, "ض");
+userName  = userName.replace(/ê/g, "ض");
+userName  = userName.replace(/è/g, "ع");
+userName  = userName.replace(/é/g, "غ");
+userName  = userName.replace(/ñ/g, "ك");
+userName  = userName.replace(/'/g, "");
+userName  = userName.replace(/I/g, "ى");
+userName  = userName.replace(/À/g, "ا");
+userName  = userName.replace(/Á/g, "ا");
+userName  = userName.replace(/Ì/g, "ي");
+userName  = userName.replace(/Í/g, "ي");
+userName  = userName.replace(/Ñ/g, "ك");
+userName  = userName.replace(/ß/g, "و");
+userName  = userName.replace(/ı/g, "ي");
+userName  = userName.replace(/i/g, "ي");
+userName  = userName.replace(/y/g, "ي");
+userName  = userName.replace(/Y/g, "ي");
+userName  = userName.replace(/î/g, "ي");
+userName  = userName.replace(/ت'/g, "ث");
+userName  = userName.replace(/وء/g, "ؤ");
+userName  = userName.replace(/يء/g, "ئ");
+userName  = userName.replace(/ءا/g, "أ");
+userName  = userName.replace(/"/g, "ع");
+userName  = userName.replace(/س'/g, "ش");
+userName  = userName.replace(/غ'/g, "غ");
+userName  = userName.replace(/ص'/g, "ض");
+userName  = userName.replace(/ط'/g, "ظ");
+userName  = userName.replace(/د'/g, "ذ");
+userName  = userName.replace(/ح'/g, "خ");
+userName  = userName.replace(/ر'/g, "ز");
+userName  = userName.replace(/ه'/g, "ة");
+userName  = userName.replace(/وو/g, "و");
+userName  = userName.replace(/0/g, "۰");
+userName  = userName.replace(/1/g, "۱");
+userName  = userName.replace(/2/g, "۲");
+userName  = userName.replace(/3/g, "۳");
+userName  = userName.replace(/4/g, "۴");
+userName  = userName.replace(/5/g, "۵");
+userName  = userName.replace(/6/g, "۶");
+userName  = userName.replace(/7/g, "۷");
+userName  = userName.replace(/8/g, "۸");
+userName  = userName.replace(/9/g, "۹");
+
+document.getElementById('nameResult').innerHTML=userName;
+
+var obj=document.getElementById('userNameTxt');
+
+obj.focus();
+
+obj.scrollTop=obj.scrollHeight;
+
+}
+
+function converToOttoman(unicode) {
+
+var convertForm = document.getElementById('userNameTxt');
+
+  if (document.selection) {
+
+convertForm.focus();
+
+sel = document.selection.createRange();
+
+sel.text = String.fromCharCode(unicode);
+
+}
+
+else if (document.getElementById('userNameTxt').selectionStart || document.getElementById('userNameTxt').selectionStart == "0") {
+
+ var startPos = document.getElementById('userNameTxt').selectionStart;
+
+var endPos = document.getElementById('userNameTxt').selectionEnd;
+
+ var chaine = document.getElementById('userNameTxt').value;
+
+convertForm.value = chaine.substring(0, startPos) + String.fromCharCode(unicode) + chaine.substring(endPos, chaine.length);
+
+} else {
+
+ convertForm.value += String.fromCharCode(unicode);
+
+}
+
+var obj=document.getElementById('userNameTxt');
+
+obj.focus();
+
+obj.scrollTop=obj.scrollHeight;
+
+ }
+
+function copy()
+
+{ textRange=document.getElementById('userNameTxt').createTextRange();   textRange.execCommand("Copy");   textRange="";
+
+}
+
